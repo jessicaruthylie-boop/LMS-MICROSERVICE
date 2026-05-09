4a7c00f9bde641d9268a03c38bd4aed0702e156a
jessicaruthylie-boop <jessicaruthylie@gmail.com>
2026-05-09 11:53:49 +0700

    ya

--- exam-service/models/Course.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const CourseSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  year: Number,
-});
-
-module.exports = mongoose.model("Course", CourseSchema);
---
+++ exam-service/models/Exam.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const examSchema = new mongoose.Schema({
+  course: String,
+
+  category: String,
+
+  title: String,
+
+  description: String,
+
+  duration: Number,
+
+  totalQuestions: Number,
+
+  passingScore: Number,
+
+  questions: [
+    {
+      question: String,
+
+      options: [String],
+
+      correctAnswer: String,
+    },
+  ],
+});
+
+module.exports = mongoose.model("Exam", examSchema);
