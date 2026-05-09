62eab83e80ede6667bcd587b3d2faab85f74f925
jessicaruthylie-boop <jessicaruthylie@gmail.com>
2026-05-09 16:25:14 +0700

    w

--- exam-service/config/postgres.js
+++ exam-service/config/postgres.js
@@ -12,4 +12,22 @@ const pool = new Pool({
   port: 5432,
 });
 
+const connectPostgres = async () => {
+  while (true) {
+    try {
+      await pool.query("SELECT NOW()");
+
+      console.log("PostgreSQL Connected");
+
+      break;
+    } catch (error) {
+      console.log("Waiting PostgreSQL...");
+
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+    }
+  }
+};
+
+connectPostgres();
+
 module.exports = pool;
