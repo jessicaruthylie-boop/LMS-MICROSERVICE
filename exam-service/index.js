6054a0711ae395cbf374ff7f110bdd2c900fcf64
jessicaruthylie-boop <jessicaruthylie@gmail.com>
2026-05-09 16:20:18 +0700

    s

--- exam-service/index.js
+++ exam-service/index.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 
 const express = require("express");
+
 const cors = require("cors");
-const mongoose = require("mongoose");
 
 const examRoutes = require("./routes/examRoutes");
 
@@ -11,50 +11,27 @@ const connectRabbitMQ = require("./config/rabbitmq");
 const app = express();
 
 app.use(cors());
-app.use("/exams", examRoutes);
+
 app.use(express.json());
 
-let channel;
+app.use("/exams", examRoutes);
 
-mongoose
-  .connect("mongodb://mongodb:27017/lms_exam_db")
-  .then(() => {
-    console.log("MongoDB Connected");
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+let channel;
 
 app.get("/", (req, res) => {
   res.send("Exam Service Running");
 });
 
-app.use("/exams", examRoutes);
-
-app.post("/submit-exam", async (req, res) => {
-  const data = {
-    student: req.body.student,
-
-    exam: req.body.exam,
-
-    status: "completed",
-  };
-
-  channel.sendToQueue("exam_queue", Buffer.from(JSON.stringify(data)));
-
-  res.json({
-    message: "Exam submitted",
-
-    data,
-  });
-});
-
 const startServer = async () => {
   channel = await connectRabbitMQ();
 
-  app.listen(process.env.PORT, () => {
-    console.log("Exam Service Running on 3004");
-  });
+  app.listen(
+    process.env.PORT,
+
+    () => {
+      console.log("Exam Service Running on 3004");
+    },
+  );
 };
 
 startServer();
