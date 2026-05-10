baaaeeb4aca13e7dab1f26ecbaa429e1506d38d1
jessicaruthylie-boop <jessicaruthylie@gmail.com>
2026-05-10 21:40:13 +0700

    d

--- exam-service/config/postgres.js
+++ exam-service/config/postgres.js
@@ -3,31 +3,13 @@ const { Pool } = require("pg");
 const pool = new Pool({
   user: "postgres",
 
-  host: "postgres",
+  host: "localhost",
 
   database: "lms_exam_db",
 
   password: "postgres",
 
-  port: 5432,
+  port: 5433,
 });
-
-const connectPostgres = async () => {
-  while (true) {
-    try {
-      await pool.query("SELECT NOW()");
-
-      console.log("PostgreSQL Connected");
-
-      break;
-    } catch (error) {
-      console.log("Waiting PostgreSQL...");
-
-      await new Promise((resolve) => setTimeout(resolve, 5000));
-    }
-  }
-};
-
-connectPostgres();
 
 module.exports = pool;
