57d282c9a4a6abecb549e57e629972a222d0bb8f
jessicaruthylie-boop <jessicaruthylie@gmail.com>
2026-05-14 20:37:41 +0700

    ad

---
+++ announcement-service/config/db.js
@@ -0,0 +1,11 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "lms_exam_db",
+  password: "postgres",
+  port: 5433,
+});
+
+module.exports = pool;
