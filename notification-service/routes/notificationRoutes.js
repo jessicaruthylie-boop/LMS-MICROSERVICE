3fccd2b108036722783ab2ef1f3c26f068e577ae
jessicaruthylie-boop <jessicaruthylie@gmail.com>
2026-05-14 21:06:23 +0700

    as

---
+++ notification-service/routes/notificationRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const router = express.Router();
+
+const {
+  getNotifications,
+  addNotification,
+} = require("../controllers/notificationController");
+
+router.get("/", getNotifications);
+
+router.post("/", addNotification);
+
+module.exports = router;
